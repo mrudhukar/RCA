be498c66cf8e57fcc4e2b180ca13370b2c4d3821
mrudhukar <mrudhukar@chronus.com>
2012-06-08 09:02:18 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rca::Application.config.secret_token = 'd5ba614c7a73830d5ba12c12a7d26887a6cefd0e278eda4873b17155d9cabb7ea1cc83d620b6b217d18f4dc046853126c50a6f619ffb2a4650851ef2f826251b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rca::Application.config.session_store :cookie_store, key: '_rca_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rca::Application.config.session_store :active_record_store
