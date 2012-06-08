1deb85c69368b865a4d243ec76d513066a07ac44
Mrudhukar <mrudhu@gmail.com>
2012-06-08 18:58:35 +0530

    RC

--- app/controllers/root_causes_controller.rb
+++ app/controllers/root_causes_controller.rb
@@ -16,7 +16,6 @@ class RootCausesController < ApplicationController
   def index
     @tab = TabConstants::ROOT_CAUSES
     @root_causes = RootCause.order("root_cause_bugs_count DESC").all
-
   end
 
   def show
