df8bb099c0a3da96c78d64625efc3324de87f34b
Mrudhukar <mrudhukar@chronus.com>
2012-06-10 11:55:41 +0530

    Adding bootstrap

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,8 @@ module Rca
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
+
     config.action_mailer.default_url_options = {:host => ENV['SITE_URL'] || "localhost:3000"}
   end
 end
